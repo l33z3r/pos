6a6b70cce92f4e0330a456e9f8961fa1b8f8e5d3
lee <lee@lee-xps-ubuntu.(none)>
2011-05-02 17:51:34 +0100

    MAde change to path in paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 require 'paperclip'
 
 #path to paperclip tools
-Paperclip.options[:command_path] = "C:/Users/Lee/work/tools/ImageMagick-6.6.9-Q16"
+#Paperclip.options[:command_path] = "C:/Users/Lee/work/tools/ImageMagick-6.6.9-Q16"
 Paperclip.options[:swallow_stderr] = false
