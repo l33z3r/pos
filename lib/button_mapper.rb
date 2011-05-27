e1e6aabfb05e6c477eca37e031fe8c652590d6ae
Lee Farrell <lee.farrell86@gmail.com>
2011-05-27 12:35:03 +0100

    Deploy new system settings table

--- lib/button_mapper.rb
+++ lib/button_mapper.rb
@@ -70,7 +70,7 @@ class ButtonMapper
     when SYSTEM_BUTTON
       @retval = "window.location = '#{admin_global_settings_path}'; return false;"
     when THEMES_BUTTON
-      @retval = "window.location = '#{admin_custom_themes_path}'; return false;"
+      @retval = "alert('not yet implemented');"#"window.location = '#{admin_custom_themes_path}'; return false;"
     when DISCOUNT_BUTTON
       @retval = "alert('discount clicked');"
     end
