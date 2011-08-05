6b81bfaa21b0b85f48b08ffcc8330caa9e01feee
Lee Farrell <lee.farrell86@gmail.com>
2011-08-05 17:25:19 +0100

    remove path to paperclip bin for heroku

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,11 @@ require 'paperclip'
 
 #path to paperclip tools, required on fergus pos linux box
 if Rails.env == "production"
+  #ferg linux and pos stick
 #  Paperclip.options[:command_path] = "/usr/local/bin"
+  
+  #heroku
+  Paperclip.options[:command_path] = "/usr/bin"
 end 
 
 Paperclip.options[:swallow_stderr] = false
