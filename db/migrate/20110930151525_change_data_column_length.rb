431b6549d0ca1664f3d2ecc4fed43c62ade7153b
Lee Farrell <lee.farrell86@gmail.com>
2011-10-03 10:53:16 +0100

    Fixed migration for postgres on heroku, so that the data type is mapped correctly AGAIN

--- db/migrate/20110930151525_change_data_column_length.rb
+++ db/migrate/20110930151525_change_data_column_length.rb
@@ -1,7 +1,7 @@
 class ChangeDataColumnLength < ActiveRecord::Migration
   def self.up
     if Rails.env.production_heroku?
-      execute("ALTER TABLE terminal_sync_data ALTER COLUMN terminal_sync_data.data TYPE TEXT")
+      execute("ALTER TABLE terminal_sync_data ALTER COLUMN data TYPE TEXT")
     else
       change_column :terminal_sync_data, :data, :longtext
     end
