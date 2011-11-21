1a6cccecf4ba3321848eebc66ffc2a806b537a5a
Leonardo Pavone <leonardopavone@gmail.com>
2011-11-21 12:08:09 +0000

    missing file

--- db/migrate/20111111152511_add_is_deleted_to_products.rb
+++ db/migrate/20111111152511_add_is_deleted_to_products.rb
@@ -1,13 +1,8 @@
 class AddIsDeletedToProducts < ActiveRecord::Migration
   def self.up
-    change_table :products do |t|
-      t.boolean :is_deleted, :default => false
-    end
+    add_column :products, :is_deleted, :boolean, :default => false
   end
-
   def self.down
-    change_table :products do |t|
-      t.remove :is_deleted
-    end
+    add_column :products, :is_deleted
   end
 end
