e0496609db6ecf12def17169b57466bd4a638510
Lee Farrell <lee.farrell86@gmail.com>
2012-02-21 11:05:45 +0000

    bug in cash totals, added transaction block

--- public/javascripts/interfaces/large_screen/cash_reports.js
+++ public/javascripts/interfaces/large_screen/cash_reports.js
@@ -149,6 +149,13 @@ function doCashTotalReport(total_type, commit) {
 }
 
 function saveCashReportCoinCount() {
+    if(cashTotalInOperation) {
+        niceAlert("Cash Total is in operation, please wait!");
+        return;
+    }
+    
+    cashTotalInOperation = true;
+    
     doCoinCounterTotal();
     doCashTotalReport(currentTotalType, false);
 }
@@ -190,7 +197,7 @@ function finishCashTotal() {
     
     doCashTotalReport(currentTotalType, true);
     
-    showMenuScreen();
+    //showMenuScreen();
     
     printReceipt(content, false);
     
