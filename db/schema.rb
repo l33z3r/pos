ac825018bfe9491e5764444ae892e7bcb2497c0b
Daniel Murphy <daniel.murphy@wattics.com>
2012-03-08 12:35:37 +0000

    all

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(:version => 20120531093052) do
     t.string   "printers",                                 :default => ""
     t.integer  "order_item_addition_grid_id"
     t.boolean  "order_item_addition_grid_id_is_mandatory", :default => false
-    t.integer  "course_num",                               :default => 0
+    t.integer  "course_num",                               :default => -1
     t.string   "kitchen_screens",                          :default => ""
   end
 
@@ -429,7 +429,7 @@ ActiveRecord::Schema.define(:version => 20120531093052) do
     t.boolean  "hide_on_printed_receipt",                  :default => false
     t.integer  "order_item_addition_grid_id"
     t.boolean  "order_item_addition_grid_id_is_mandatory", :default => false
-    t.integer  "course_num",                               :default => 0
+    t.integer  "course_num",                               :default => -1
     t.boolean  "is_stock_item",                            :default => true
     t.string   "kitchen_screens",                          :default => ""
     t.float    "half_price",                               :default => 0.0
