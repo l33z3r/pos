4d5c547f463d59ea9e0a7deacd326a045e2774d5
Lee Farrell <lee.farrell86@gmail.com>
2012-06-19 10:28:42 +0100

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,8 +37,8 @@
   [54, "Kitchen Screen"], [55, "X/Z History"], [56, "Double"], [57, "Table Name"], [58, "Reports"], 
   [59, "Split Bill"], [60, "Exit App"], [61, "Void Item"], [62, "Void All Items"], [63, "Delete Order"], 
   [64, "Change Prices"], [65, "Charge Card"], [66, "Covers"], [67, "Product Info"], 
-  [68, "Customers"], [69, "Loyalty Customers"], [70, "Half"], [71, "PM_SHORTCUT_1_BUTTON"], 
-  [72, "PM_SHORTCUT_2_BUTTON"], [73, "PM_SHORTCUT_3_BUTTON"]
+  [68, "Customers"], [69, "Loyalty Customers"], [70, "Half"], [71, "Payment Method Button 1"], 
+  [72, "Payment Method Button 2"], [73, "Payment Method Button 3"]
 ]
 
 #now create the buttons and also init a button role for admin user
