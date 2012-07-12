898357a92111df24f88edad96b32bacfabbe0613
Lee Farrell <lee.farrell86@gmail.com>
2012-07-12 14:21:51 +0100

    bug in migration

--- db/migrate/20120712122428_order_data_migrate.rb
+++ db/migrate/20120712122428_order_data_migrate.rb
@@ -4,7 +4,7 @@ class OrderDataMigrate < ActiveRecord::Migration
     execute("update order_items set void_employee_id = employee_id where void_employee_id is null")
     
     #when we added the loyalty system, the old cash totals had no loyalty info
-    CashTotal.all.each do |ct|
+    CashTotal.where("total_type = '#{CashTotal::X_TOTAL}' or total_type = '#{CashTotal::Z_TOTAL}'").each do |ct|
       if !ct.report_data[:loyalty_redeemed]
         ct.report_data[:loyalty_redeemed] = 0
         ct.save
