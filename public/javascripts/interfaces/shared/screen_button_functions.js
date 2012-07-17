bbdffd87c7fc1cf074d1fe8d3d006cc3393a0745
Daniel Murphy <daniel.murphy@wattics.com>
2012-07-17 17:43:45 +0100

    new report changes

--- public/javascripts/interfaces/shared/screen_button_functions.js
+++ public/javascripts/interfaces/shared/screen_button_functions.js
@@ -3,7 +3,48 @@ var orderInProcess = false;
 
 var isTableZeroOrder = false;
 
+function orderButtonPressed() {
+    var order = getCurrentOrder();
+
+    var autoCovers = false;
+
+    if (globalAutoPromptForCovers) {
+        autoCovers = true;
+    } else {
+        //iterate through all the categories of this order to check for auto covers set
+        for (var j = 0; j < order.items.length; j++) {
+            var item = order.items[j];
+
+            if (order.items[j].synced && selectedTable != 0) {
+                continue;
+            }
+
+            var categoryId = item.product.category_id;
+
+            if (categoryId != null) {
+                if (categories[categoryId].prompt_for_covers) {
+                    autoCovers = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (autoCovers && parseInt(order.covers) == -1) {
+        order.covers = 0;
+        manualCoversPrompt = false;
+        doAutoCovers();
+    } else {
+        doSyncTableOrder();
+    }
+}
+
 function doSyncTableOrder() {
+    if (!appOnline) {
+        niceAlert("Cannot contact server, ordering is disabled until connection re-established!");
+        return;
+    }
+
     var order = null;
     
     if(!isTableZeroOrder && !ensureLoggedIn()) {
