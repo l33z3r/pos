30b06fd26387ab801e5dbf7b74c3a5096b365e0d
Daniel Murphy <daniel.murphy@wattics.com>
2012-08-14 18:13:14 +0100

    bug fixes on payments

--- app/controllers/reports/glances_controller.rb
+++ app/controllers/reports/glances_controller.rb
@@ -3,261 +3,177 @@ class Reports::GlancesController < Admin::AdminController
   layout 'reports'
 
   def index
-    glances_search
-    last_z_total
-  end
+    session[:search_type] = :best_seller
+    session[:search_type_label] = "Best Seller"
+    session[:category] = ''
+    session[:product] = ''
+    session[:terminal] = ''
+    session[:search_product] = ''
+    session[:from_date] = Time.now.beginning_of_day
+    session[:to_date] = Time.now.end_of_day
+    session[:terminal] = ''
+    session[:training_mode] = false
 
-  def last_z_total
-    @last_z_total = CashTotal.where("total_type = ?","Z").order("created_at DESC").first
-    if(@last_z_total)
-      @last_z_total_by = @last_z_total.employee.nickname
-      @last_z_total_date = @last_z_total.created_at.to_date.strftime("%d %b %Y")
-      @last_z_total_time = @last_z_total.created_at.to_time.strftime("%H:%M")
-    end
-  end
+    session[:preselect] = -1
 
-  def glances_search
     @opening_time = GlobalSetting.parsed_setting_for GlobalSetting::EARLIEST_OPENING_HOUR
     @closing_time = GlobalSetting.parsed_setting_for GlobalSetting::LATEST_CLOSING_HOUR
-    @selected_from_date = Date.today.midnight + @opening_time.hours
-    @selected_to_date = Date.tomorrow.midnight + @closing_time.hours
+
+
+
+    @current_category = nil
+    @current_product = nil
     @all_terminals = all_terminals
+      #sales_search
+    glances_search
+
+  end
+
+
+  def glances_search
+
    # get_sales_data
-    @orders = get_sales_data
-    iterate_orders(@orders)
-    calculate_summary    
-    if !@orders.nil?
-      create_sales_per_hour_graph
-      get_busiest_hour
-    end
+    @today_sales = todays_sales
+    @average_day_sales = average_day_sales
+    @total_number_days = total_number_days
+    @number_of_sales = number_of_sales
+    @total_number_sales = total_number_sales
+    @busiest_hour = busiest_hour
+    @total_busiest_hour = total_busiest_hour
+
   end
 
-  def iterate_orders orders
-    @sales_per_hour = Hash.new { |h,k| h[k] = 0.0 }
-    @items_per_interval_hour = Hash.new { |h,k| h[k] = [0,0.0] }
-    @items_per_hour = Hash.new { |h,k| h[k] = [0,0.0] }
-    @sales_by_payment_type = Hash.new { |h,k| h[k] = 0.0 }
-    @total_discount = 0.0
-    @average_sale = 0.0
-    @total_of_sales = 0.0
-    @cash_paid_out_total = 0.0
-    @sales_last_7_days_total = 0.0
-    @sales_by_payment_total = 0.0
-    @sales_by_server_total = 0.0
-    @sales_by_department_total = 0.0
-    @voided_sales = @number_of_sales = 0
-    @sales_by_server = Hash.new { |h,k| h[k] = 0.0 }
-    @top_selling_items = Hash.new { |h,k| h[k] = ["",0,0.0] }
-    @sales_by_department = Hash.new { |h,k| h[k] = 0.0 }
-    @sales_last_7_days = Hash.new { |h,k| h[k] = ["",0.0] }
-    
-    for order in orders
-      if !order.is_void and check_interval_hour(order)
-        #iterate order items
-        iterate_order_items(order)
-        #sales per hour for graph
-        calculate_sales_per_hour(order)
-        #sales by payment type
-        calculate_sales_by_payment_type(order)
-        #by server
-        calculate_sales_by_server(order)
-        #misc data
-        calculate_misc_values(order)
+
+  def set_params
+    session[:current_page] = 1
+    if params[:search][:select_type] != ''
+      session[:preselect] = params[:search][:select_type].to_i
+    else
+      session[:preselect] = 0
+    end
+
+    if params[:search][:training_mode] == 'true'
+      session[:training_mode] = true
+    else
+      session[:training_mode] = false
+    end
+
+    if params[:search][:search_type] == 'day'
+      session[:search_type] = :day
+      session[:search_type_label] = 'By Day'
+    elsif params[:search][:search_type] == 'week'
+      session[:search_type] = :week
+      session[:search_type_label] = 'By Week'
+    elsif params[:search][:search_type] == 'month'
+      session[:search_type] = :month
+      session[:search_type_label] = 'By Month'
+    elsif params[:search][:search_type] == 'year'
+      session[:search_type] = :year
+      session[:search_type_label] = 'By Year'
+    elsif params[:search][:search_type] == 'employee'
+      session[:search_type] = :employee
+      session[:search_type_label] = 'By Employee'
+    end
+    if params[:search][:from_date]
+      session[:from_date] = params[:search][:from_date]
+    end
+    if params[:search][:to_date]
+      session[:to_date] = params[:search][:to_date]
+    end
+    if params[:search][:terminal]
+      session[:terminal] = params[:search][:terminal]
+    end
+    render :nothing => true
+  end
+
+
+
+  def todays_sales
+    @selected_from_date = session[:from_date].to_s
+    @selected_to_date = session[:to_date].to_s
+    where = "select oi.id, oi.created_at, DATE_FORMAT(oi.created_at,'%Y-%m-%d') as created_day, oi.product_id, SUM((oi.total_price-(oi.total_price/(1+(oi.tax_rate/100))))) as tax_rate, SUM(total_price) total_price, SUM(quantity) quantity from order_items oi inner join orders o on oi.order_id = o.id"
+    if session[:terminal] != ''
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0 and o.terminal_id = '#{session[:terminal]}'"
       else
-        @voided_sales += 1
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0"
+    end
+    query = OrderItem.find_by_sql(where)[0].total_price
+  end
+
+  def average_day_sales
+    @selected_from_date = Time.now.beginning_of_year
+    @selected_to_date = Time.now
+    where = "select oi.id, oi.created_at, DATE_FORMAT(oi.created_at,'%Y-%m-%d') as created_day, oi.product_id, SUM((oi.total_price-(oi.total_price/(1+(oi.tax_rate/100))))) as tax_rate, SUM(total_price) total_price, day(oi.created_at), SUM(quantity) quantity from order_items oi inner join orders o on oi.order_id = o.id"
+    if session[:terminal] != ''
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0 and o.terminal_id = '#{session[:terminal]}'"
+      else
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0"
       end
-    end
-    @top_selling_items = @top_selling_items.sort_by { |k, v| v[1] }.reverse.first(5)
+     #where << " group by created_day order by oi.created_at asc"
+    query = OrderItem.find_by_sql(where)[0].total_price
   end
 
-  def check_interval_hour order
-    order_hour = order.created_at.hour
-    if @hour_interval_from.nil? and @hour_interval_to.nil?
-      return true
-    elsif @hour_interval_from.nil? and order_hour.to_i < @hour_interval_to.to_i
-      return true
-    elsif @hour_interval_to.nil? and order_hour.to_i >= @hour_interval_from.to_i
-      return true
-    elsif @hour_interval_from.to_i <= @hour_interval_to.to_i and order_hour.to_i >= @hour_interval_from.to_i and order_hour.to_i < @hour_interval_to.to_i
-      return true
-    elsif @hour_interval_from.to_i > @hour_interval_to.to_i and (order_hour.to_i >= @hour_interval_from.to_i or order_hour.to_i < @hour_interval_to.to_i)
-      return true
-    end
-    return false
-  end
-
-  def get_busiest_hour
-    if !@items_per_hour.nil? and !@items_per_hour.empty?
-      @busiest_hour = @items_per_hour.sort_by { |k, v| v }.reverse.first[0]
-    end
-  end
-
-  def calculate_misc_values order
-    @number_of_sales += 1
-    @total_of_sales += order.total
-    @average_sale = @total_of_sales / @number_of_sales
-    if !order.pre_discount_price.nil? and !order.discount_percent.eql?(0)
-      @total_discount += order.pre_discount_price - order.total
-    end
-  end
-
-  def calculate_summary
-    date_to = Date.today.tomorrow.midnight + @closing_time.hours
-    date_from = Date.today.prev_day(6).midnight + @opening_time.hours
-    @summary_orders = Order.where("created_at >= ? AND created_at < ? AND is_void = ?", date_from, date_to, false)
-    for order in @summary_orders
-      #check if the order is of the past day after 24 hs
-      if order.created_at.to_time.strftime("%H").to_i < @opening_time
-        order.created_at = order.created_at.to_date - 1.day
+  def total_number_days
+    @selected_from_date = Time.now.beginning_of_year
+    @selected_to_date = Time.now
+    where = "select oi.id, oi.created_at, DATE_FORMAT(oi.created_at,'%Y-%m-%d') as created_day, oi.product_id, SUM((oi.total_price-(oi.total_price/(1+(oi.tax_rate/100))))) as tax_rate, SUM(total_price) total_price, day(oi.created_at), SUM(quantity) quantity from order_items oi inner join orders o on oi.order_id = o.id"
+    if session[:terminal] != ''
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0 and o.terminal_id = '#{session[:terminal]}'"
+      else
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0"
       end
-      if(@sales_last_7_days[order.created_at.to_date.to_s][0].eql?(""))
-        @sales_last_7_days[order.created_at.to_date.to_s][0] = order.created_at.to_date.strftime("%A")
+     where << " group by created_day order by oi.created_at asc"
+    query = OrderItem.find_by_sql(where).count
+  end
+
+  def number_of_sales
+    @selected_from_date = Time.now.beginning_of_day
+    @selected_to_date = Time.now
+    where = "select oi.id, oi.created_at from order_items oi inner join orders o on oi.order_id = o.id"
+    if session[:terminal] != ''
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0 and o.terminal_id = '#{session[:terminal]}'"
+      else
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0"
       end
-      @sales_last_7_days[order.created_at.to_date.to_s][1] += order.total
-      @sales_last_7_days_total += order.total
-    end
-    if @sales_last_7_days[Date.today.to_s] 
-      @sales_last_7_days[Date.today.to_s][0] = "Today"
-    end
-    if @sales_last_7_days[Date.today.prev_day(1).to_s]
-      @sales_last_7_days[Date.today.prev_day(1).to_s][0] = "Yesterday"
-    end
+    query = OrderItem.find_by_sql(where).count
   end
 
-  def calculate_sales_by_server order
-    @sales_by_server[order.employee.nickname] += order.total
-    @sales_by_server_total += order.total
-  end   
-
-  def calculate_sales_by_payment_type order
-    @sales_by_payment_type[order.payment_type] += order.total
-    @sales_by_payment_total += order.total
-  end
-
-  def calculate_sales_per_hour order
-    order_hour = order.created_at.to_time.hour
-    @sales_per_hour[order_hour] += order.total
-    @sales_per_hour[order_hour] = @sales_per_hour[order_hour].round(2)
-  end
-
-  def iterate_order_items order
-    for order_item in order.order_items
-      calculate_top_selling_items(order_item)
-      calculate_items_by_hour(order_item)
-      calculate_sales_by_department(order_item)
-      calculate_discount_by_item(order_item)
-    end
-  end
-
-  def calculate_discount_by_item order_item
-    if !order_item.pre_discount_price.nil? and order_item.discount_percent > 0
-      @total_discount += order_item.pre_discount_price - order_item.total_price
-    end
-  end
-
-  def calculate_top_selling_items order_item
-    @top_selling_items[order_item.product_id][0] = order_item.product_name
-    @top_selling_items[order_item.product_id][1] += order_item.quantity.to_i
-    @top_selling_items[order_item.product_id][2] += order_item.total_price 
-  end
-
-  def calculate_items_by_hour order_item
-    order_hour = order_item.order.created_at.hour
-    @items_per_hour[order_hour][0] += order_item.quantity.to_i
-    @items_per_hour[order_hour][1] += order_item.total_price
-  end
-
-  def calculate_sales_by_department order_item
-    if order_item.product and order_item.product.category and order_item.product.category.parent_category
-      department_name = order_item.product.category.parent_category.name
-    else
-      department_name = "No Dept."
-    end
-    @sales_by_department[department_name] += order_item.total_price 
-    @sales_by_department_total += order_item.total_price 
-  end
-
-  def create_sales_per_hour_graph 
-    #depending of the quantity hour items, show it at intervals or in plain hours
-    x_axis = Array.new
-    values = Array.new
-    max_value = 0.0
-    if @items_per_hour.length <= 15
-      #items per hour table
-      @items_per_interval_hour = @items_per_hour
-      #graph's axis
-      x_axis = @sales_per_hour.keys.sort
-      for item in @sales_per_hour.sort_by { |k, v| k }
-        values.push(item[1])
+  def total_number_sales
+    @selected_from_date = Time.now.beginning_of_year
+    @selected_to_date = Time.now
+    where = "select oi.id, oi.created_at from order_items oi inner join orders o on oi.order_id = o.id"
+    if session[:terminal] != ''
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0 and o.terminal_id = '#{session[:terminal]}'"
+      else
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0"
       end
-      @h_interval = 1
-      max_value = @sales_per_hour.values.max_by { |obj| obj }
-    else
-      #items per hour
-      sum_total = 0.0
-      sum_qty = 0.0
-      total_sales = 0.0
-      for i in 0..23
-        sum_qty += (!@items_per_hour[i].nil?) ? @items_per_hour[i][0] : 0
-        sum_total += (!@items_per_hour[i].nil?) ? @items_per_hour[i][1] : 0
-        total_sales += (!@sales_per_hour[i].nil?) ? @sales_per_hour[i] : 0
-        if(i % 2 != 0)
-          @items_per_interval_hour[i-1][0] += sum_qty.to_i
-          @items_per_interval_hour[i-1][1] += sum_total
-          values.push(total_sales.round(2))
-          total_sales = 0.0
-          sum_qty = 0.0
-          sum_total = 0.0
-        end        
-      end
-      #graph's axis
-      x_axis = ["0","2","4","6","8","10","12","14","16","18","20","22"]
-      @h_interval = 2
-      max_value = values.max_by { |v| v }
-    end
-
-    #start to show respect the opening time
-    if @h_interval == 1
-      @elems_to_move = x_axis.select { |elem| elem.to_i < @opening_time }.length
-    else
-      @elems_to_move = x_axis.select { |elem| elem.to_i < @opening_time and elem.to_i+1 < @opening_time }.length
-    end
-    x_axis = x_axis.rotate(@elems_to_move)
-    values = values.rotate(@elems_to_move)
-
-    @currency = GlobalSetting.parsed_setting_for GlobalSetting::CURRENCY_SYMBOL
-    @graph = LazyHighCharts::HighChart.new('graph') do |f|
-      f.options[:chart][:defaultSeriesType] = "column"
-      f.options[:chart][:animation] = true
-      f.options[:title][:text] = "Sales Per Hour"
-      f.options[:xAxis][:categories] = x_axis
-      f.options[:yAxis][:min] = "0"
-      f.options[:yAxis][:max] = max_value
-      f.options[:yAxis][:title][:text] = "Total (#{@currency})"
-      f.series(:name=>'Sales', :data=> values )
-    end
+    query = OrderItem.find_by_sql(where).count
   end
 
-  def get_sales_data
-    if !params[:search]
-      query = Order.where("created_at >= ? AND created_at < ?", @selected_from_date, @selected_to_date)
-    else
-      if params[:search2] and params[:search2][:hour_from]
-        @hour_interval_from = params[:search2][:hour_from]
+  def busiest_hour
+    @selected_from_date = Time.now.beginning_of_day
+    @selected_to_date = Time.now
+    where = "select oi.id, oi.created_at, oi.product_id, SUM((oi.total_price-(oi.total_price/(1+(oi.tax_rate/100))))) as tax_rate, SUM(total_price) total_price, SUM(quantity) quantity from order_items oi inner join orders o on oi.order_id = o.id"
+    if session[:terminal] != ''
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0 and o.terminal_id = '#{session[:terminal]}'"
+      else
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0"
       end
-      if params[:search2] and params[:search2][:hour_to]
-        @hour_interval_to = params[:search2][:hour_to]
-      end
-      if params[:search] and params[:search][:created_at_gt] and !params[:search][:created_at_gt].empty?
-        @selected_from_date = params[:search][:created_at_gt]
-        params[:search][:created_at_gt] = @selected_from_date.to_date.midnight + @opening_time.hours
-      end
-      if params[:search] and params[:search][:created_at_lt] and !params[:search][:created_at_lt].empty?
-        @selected_to_date = params[:search][:created_at_lt]
-        params[:search][:created_at_lt] = @selected_to_date.to_date.tomorrow.midnight + @closing_time.hours
-      end
-      query = Order.search(params[:search])
-    end
-    return query
+    where << " group by hour(oi.created_at) order by total_price desc"
+    query = OrderItem.find_by_sql(where)[0].created_at
   end
+
+  def total_busiest_hour
+    @selected_from_date = Time.now.beginning_of_year
+    @selected_to_date = Time.now
+    where = "select oi.id, oi.created_at, oi.product_id, SUM((oi.total_price-(oi.total_price/(1+(oi.tax_rate/100))))) as tax_rate, SUM(total_price) total_price, SUM(quantity) quantity from order_items oi inner join orders o on oi.order_id = o.id"
+    if session[:terminal] != ''
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0 and o.terminal_id = '#{session[:terminal]}'"
+      else
+        where << " where oi.created_at <= '#{@selected_to_date}' and oi.created_at >= '#{@selected_from_date}' and o.is_void = 0 and oi.is_void = 0"
+      end
+    where << " group by hour(oi.created_at) order by total_price desc"
+    query = OrderItem.find_by_sql(where)[0].created_at
+  end
+
 end
