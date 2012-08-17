2c17f45776add8857f12db006d350c3f02532281
Lee Farrell <lee.farrell86@gmail.com>
2012-08-17 16:18:37 +0100

    new log separation

--- config/initializers/custom_logs.rb
+++ config/initializers/custom_logs.rb
@@ -1,21 +1,21 @@
-class CustomLogger < Rails::Rack::Logger
-  def initialize(app, opts = {})
-    @app = app
-    @opts = opts
-    @opts[:silenced] ||= []
-  end
-
-  def call(env)
-    if @opts[:silenced].include?(env['PATH_INFO'])
-      Rails.logger.silence do
-        @app.call(env)
-      end
-    else
-      super(env)
-    end
-  end
-end
-
-silenced_actions = ["/call_home.js", "/ping", "/last_receipt_for_terminal.js", "/last_receipt_for_server.js", "/last_receipt_for_table.js", "/blank_receipt_for_print"]
-
-Rails.configuration.middleware.swap Rails::Rack::Logger, CustomLogger, :silenced => silenced_actions
+#class CustomLogger < Rails::Rack::Logger
+#  def initialize(app, opts = {})
+#    @app = app
+#    @opts = opts
+#    @opts[:silenced] ||= []
+#  end
+#
+#  def call(env)
+#    if @opts[:silenced].include?(env['PATH_INFO'])
+#      Rails.logger.silence do
+#        @app.call(env)
+#      end
+#    else
+#      super(env)
+#    end
+#  end
+#end
+#
+#silenced_actions = ["/call_home.js", "/ping", "/last_receipt_for_terminal.js", "/last_receipt_for_server.js", "/last_receipt_for_table.js", "/blank_receipt_for_print"]
+#
+#Rails.configuration.middleware.swap Rails::Rack::Logger, CustomLogger, :silenced => silenced_actions
