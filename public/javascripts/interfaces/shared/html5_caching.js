a48eced69fd9890e454eb815cc8e47d3beb1a5a7
Lee Farrell <lee.farrell86@gmail.com>
2012-09-26 16:30:22 +0100

    more changes

--- public/javascripts/interfaces/shared/html5_caching.js
+++ public/javascripts/interfaces/shared/html5_caching.js
@@ -17,7 +17,7 @@ $(window).bind(
     function(event) {
         setConnectionStatus(navigator.onLine);
     }
-    );
+);
  
 // List for checking events. This gets fired when the browser
 // is checking for an udpated manifest file or is attempting
@@ -27,7 +27,7 @@ $(appCache).bind(
     function(event) {
         console.log("Checking for manifest");
     }
-    );
+);
  
 // This gets fired if there is no update to the manifest file
 // that has just been checked.
@@ -36,7 +36,7 @@ $(appCache).bind(
     function(event) {
         console.log("No cache updates");
     }
-    );
+);
  
 // This gets fired when the browser is downloading the files
 // defined in the cache manifest.
@@ -52,7 +52,7 @@ $(appCache).bind(
         // Get the total number of files in our manifest.
         getTotalFiles();
     }
-    );
+);
  
 // This gets fired for every file that is downloaded by the
 // cache update.
@@ -76,7 +76,7 @@ $(appCache).bind(
         cacheDownloading = false;
         cacheDownloadReset();
     }
-    );
+);
  
 // This gets fired when new cache files have been downloaded
 // and are ready to replace the *existing* cache. The old
@@ -104,7 +104,7 @@ $(appCache).bind(
 $(appCache).bind(
     "error",
     function(event) {
-        niceAlert("An error occurred while downloading the latest cache. You may be using a stale version.");
+        niceAlert("An error occurred while downloading the latest cache. You may be using a stale version. Try reloading the app.");
         
         //reset some things
         cacheDownloading = false;
