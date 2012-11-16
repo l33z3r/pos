650c62939beb93d9787d03a1d4a9cb3441f6c032
Lee Farrell <lee.farrell86@gmail.com>
2012-11-16 16:08:11 +0000

    bla

--- db/migrate/20121114190331_convert_time_added_cols_to_date_cols.rb
+++ db/migrate/20121114190331_convert_time_added_cols_to_date_cols.rb
@@ -1,26 +1,24 @@
 class ConvertTimeAddedColsToDateCols < ActiveRecord::Migration
   def self.up
-#    add_column :order_items, :date_added, :datetime
-#    
-#    OrderItem.all.each do |oi|
-#      oi.date_added = Time.at(oi.time_added.to_i/1000)
-#      oi.save
-#    end
-#    
-#    remove_column :order_items, :time_added
-#    
-#    add_column :orders, :date_started, :datetime
-#    
-#    Order.all.each do |o|
-#      o.date_started = Time.at(o.time_started.to_i/1000)
-#      o.save
-#    end
-#    
-#    remove_column :orders, :time_started
+    add_column :order_items, :date_added, :datetime
+    
+    OrderItem.all.each do |oi|
+      oi.date_added = Time.at(oi.time_added.to_i/1000)
+      oi.save
+    end
+    
+    remove_column :order_items, :time_added
+    
+    add_column :orders, :date_started, :datetime
+    
+    Order.all.each do |o|
+      o.date_started = Time.at(o.time_started.to_i/1000)
+      o.save
+    end
+    
+    remove_column :orders, :time_started
   end
 
   def self.down
-    remove_column :order_items, :date_added, :datetime
-    add_colu
   end
 end
--- db/migrate/20121116113636_add_timezone_to_cluey_accounts_and_outlets.rb
+++ db/migrate/20121116113636_add_timezone_to_cluey_accounts_and_outlets.rb
@@ -1,17 +1,17 @@
 class AddTimezoneToClueyAccountsAndOutlets < ActiveRecord::Migration
   def self.up
-#    add_column :cluey_accounts, :time_zone, :string, :null => false
-#    add_column :outlets, :time_zone, :string, :null => false
-#    
-#    ClueyAccount.all.each do |ca|
-#      ca.time_zone = "Dublin"
-#      ca.save
-#    end
-#    
-#    Outlet.all.each do |o|
-#      o.time_zone = "Dublin"
-#      o.save
-#    end
+    add_column :cluey_accounts, :time_zone, :string, :null => false
+    add_column :outlets, :time_zone, :string, :null => false
+    
+    ClueyAccount.all.each do |ca|
+      ca.time_zone = "Dublin"
+      ca.save
+    end
+    
+    Outlet.all.each do |o|
+      o.time_zone = "Dublin"
+      o.save
+    end
     
   end
 
