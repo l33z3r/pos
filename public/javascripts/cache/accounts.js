7cbf7d386ac411a5e8f99395ce427ac90e5c66cb
Lee Farrell <lee.farrell86@gmail.com>
2012-11-16 15:19:56 +0000

    cache_assets

--- public/javascripts/cache/accounts.js
+++ public/javascripts/cache/accounts.js
@@ -14,3 +14,4 @@ $('a[data-confirm], a[data-method], a[data-remote]').live('click.rails',function
 signInPopupAnchor.CreateBubblePopup();var signInPopupHTML=$("#signin_popup_markup").html();signInPopupAnchor.ShowBubblePopup({position:'bottom',align:'right',tail:{align:'right'},innerHtml:signInPopupHTML,innerHtmlStyle:{'text-align':'left'},themeName:'all-grey',themePath:'/images/jquerybubblepopup-theme',alwaysVisible:false},false);signInPopupAnchor.FreezeBubblePopup();signInPopupID=signInPopupAnchor.GetBubblePopupID();$(window).bind('keypress',function(event){if(event.keyCode==13){goToAccountsPage();}});}
 function goToAccountsPage(){var accountName=$('#'+signInPopupID).find('#account_name').val();if(accountName.length==0){niceAlert("Please Enter An Account Name");return;}
 window.location="http://"+accountName+"."+APP_DOMAIN+APP_PORT+"/account_log_in";}
+function niceAlert(msg){alert(msg);}
