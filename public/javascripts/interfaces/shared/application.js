29e0456feee24248599f5fc98c1db75005ac3e31
Lee Farrell <lee.farrell86@gmail.com>
2013-08-14 16:49:43 +0100

    fix bug in ping

--- public/javascripts/interfaces/shared/application.js
+++ public/javascripts/interfaces/shared/application.js
@@ -209,7 +209,7 @@ function preventOfflineHref() {
 
 function pingHome() {
     $.ajax({
-        url: "/ping",
+        url: "/ping" + new Date().getTime(),
         type : "GET",
         success: function() {
             setConnectionStatus(true);
